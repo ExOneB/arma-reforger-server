4a39041fb622c304a34e6aa453620f1e750cf925
ExOneB <ex1bka@gmail.com>
2025-03-14 12:58:38 +0300

    fix

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ cp config.json.template config.json
 
 # Replacing placeholders
 sed -i "s|__ARMA_BIND_ADDRESS__|${ARMA_BIND_ADDRESS}|g" config.json
-sed -i "s|\"__ARMA_BIND_PORT__\"|${ARMA_BIND_PORT}|g" config.json
-sed -i "s|\"__ARMA_REGISTER_PORT__\"|${ARMA_REGISTER_PORT}|g" config.json
+sed -i "s|__ARMA_PUBLIC_ADDRESS__|${ARMA_PUBLIC_ADDRESS}|g" config.json
 sed -i "s|__ARMA_ADMIN_PASSWORD__|${ARMA_ADMIN_PASSWORD}|g" config.json
+sed -i "s|\"__ARMA_BIND_PORT__\"|${ARMA_BIND_PORT}|g" config.json
 
 # Starting the server
 ./ArmaReforgerServer -config ./config.json -profile ./profile -maxFPS 60
